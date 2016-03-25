b158ed5b764963fc56055f8fb6b75b8542368c11
maricasals <casals7@gmail.com>
2016-03-25 09:22:22 +0100

    Versio ini

--- src/main/java/streaming_springmvc/spring/SpringConfig.java
+++ src/main/java/streaming_springmvc/spring/SpringConfig.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package streaming.spring;
+package streaming_springmvc.spring;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -28,8 +28,8 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 //@SpringBootApplication
 //@EnableAutoConfiguration
-@ComponentScan(basePackages = "streaming") 
-@EnableJpaRepositories(basePackages = "streaming")
+@ComponentScan(basePackages = "streaming_springmvc") 
+@EnableJpaRepositories(basePackages = "streaming_springmvc")
 @EnableWebMvc
 public class SpringConfig extends WebMvcConfigurerAdapter{
 
